4442c59826eb84d6c1aa7aa688ed2c328bffab85
roseavila0 <rosa.avilacaliz@gmail.com>
2024-12-22 20:55:18 -0500

    Agregando archivos para inciar el trabajo integrador

---
+++ trabajoPracticoIntegrador_AvilaRosa.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+//1. Estructura de Datos
+//a) Crear un array de objetos llamado libros que contenga al menos 10 libros. Cada libro debe tener las siguientes propiedades:
+//✓ id (número) ✓ título (string) ✓ autor (string) ✓ año (número) ✓ género (string) ✓ disponible (booleano).
+//b) Crear un array de objetos llamado usuarios con al menos 5 usuarios.
+//Cada usuario debe tener: ✓ id (número) ✓ nombre (string) ✓ email (string) ✓ librosPrestados (array de ids de libros).
+
+let = libros [
+    id (número)
+    título (string),
+    autor (string),
+    año (número),
+    género (string),
+    disponible (booleano).
+]
